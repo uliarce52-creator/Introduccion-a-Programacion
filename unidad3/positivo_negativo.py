0e2d64f04aa9d77aefabca5e95aff59aab713560
uliarce52-creator <uliarce52@gmail.com>
2026-04-13 18:37:01 -0300

    Add files via upload

---
+++ unidad3/positivo_negativo.py
@@ -0,0 +1,8 @@
+def positivo_negativo():
+    x = int(input("Ingrese un numero: "))
+    if x > 0:
+       print("Numero Positivo")
+    else:
+         print("Numero no positivo")
+         
+positivo_negativo()
---
+++ unidad3/pruebaIF.py
@@ -0,0 +1,6 @@
+def positivo():
+    x = int(input("Ingrese un numero: "))
+    if x > 0:
+        print("Numero Positivo")
+
+positivo()
