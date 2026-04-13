51ac7a96aa65eeb7185246eea7b36dc98ee1ca89
uliarce52-creator <uliarce52@gmail.com>
2026-04-13 18:26:32 -0300

    Add files via upload

---
+++ tp5/punto1.py
@@ -0,0 +1,7 @@
+def positivo():
+	x = int(input(f"Ingrese numero: "))
+	if x > 10:
+		print(f"Su numero es mayor a 10")
+		print(f"Chau hasta la proxima")
+	
+positivo()
---
+++ tp5/punto2.py
@@ -0,0 +1,10 @@
+def positivo():
+	x = int(input(f"Ingrese numero: "))
+	if x > 10:
+		print(f"Su numero es mayor a 10")
+		print(F"Chau hasta la proxima")
+	else:
+	    print(f"Su numero no es mayor a 10")
+	    print(F"Chau hasta la proxima")
+	
+positivo()
